684fb5b681d339859a9239ffff9b5f8a8bb2c625
Charan Marripudi <charanmarripudi@github.com>
2026-08-25 10:52:13 +0530

    Restore Thursday's liveSyncPosition snap on bufferStalledError to instantly bypass browser loading/buffering symbols

--- static/js/detection.js
+++ static/js/detection.js
@@ -72,6 +72,9 @@ function playHLS(video, url, idx) {
 
     hls.on(Hls.Events.ERROR, (_, data) => {
         if (data.details === 'bufferStalledError') {
+            if (hls.liveSyncPosition && Number.isFinite(hls.liveSyncPosition)) {
+                try { video.currentTime = hls.liveSyncPosition; } catch (_) {}
+            }
             if (video.paused) {
                 video.play().catch(() => {});
             }
