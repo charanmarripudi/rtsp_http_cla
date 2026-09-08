8c87d8d981b227df648dfacef4985de05c74fefb
Charan Marripudi <charanmarripudi@github.com>
2026-09-08 19:00:55 +0530

    fix(class-filtering): derive enabled_classes directly from DOM model cards on Start click to guarantee unassigned classes are excluded

--- static/js/detection.js
+++ static/js/detection.js
@@ -385,6 +385,20 @@ function renderUI(box, i, meta, status, cameraModelsMap) {
             models = cm[camStr] || [];
         }
         if (!models.length) return alert("No active models or classes assigned");
+        const domEnabledClasses = {};
+        box.querySelectorAll(".model-card-box").forEach(card => {
+            const mName = card.getAttribute("data-model");
+            const cls = card.getAttribute("data-class");
+            if (mName && cls) {
+                const norm = mName.endsWith(".pt") ? mName : `${mName}.pt`;
+                const clean = mName.replace(".pt", "");
+                domEnabledClasses[norm] = domEnabledClasses[norm] || [];
+                if (!domEnabledClasses[norm].includes(cls)) domEnabledClasses[norm].push(cls);
+                domEnabledClasses[clean] = domEnabledClasses[clean] || [];
+                if (!domEnabledClasses[clean].includes(cls)) domEnabledClasses[clean].push(cls);
+            }
+        });
+
         box.querySelectorAll(".model-card-box").forEach(card => {
             const mName = card.getAttribute("data-model");
             const cls = card.getAttribute("data-class");
@@ -393,25 +407,25 @@ function renderUI(box, i, meta, status, cameraModelsMap) {
             if (mName && cSlider && iSlider) {
                 const cVal = parseFloat(cSlider.value);
                 const iVal = parseFloat(iSlider.value);
+                const norm = mName.endsWith(".pt") ? mName : `${mName}.pt`;
                 const clean = mName.replace(".pt", "");
                 
-                const origClasses = (meta.model_configs && meta.model_configs[mName] && meta.model_configs[mName].enabled_classes) ||
-                                    (meta.model_configs && meta.model_configs[clean] && meta.model_configs[clean].enabled_classes);
+                const activeClasses = domEnabledClasses[norm] || domEnabledClasses[clean] ||
+                                    (meta.model_configs && meta.model_configs[norm] && meta.model_configs[norm].enabled_classes) ||
+                                    (meta.model_configs && meta.model_configs[clean] && meta.model_configs[clean].enabled_classes) || [];
                 if (cls) {
-                    domModelConfigs[mName] = domModelConfigs[mName] || {};
-                    if (origClasses) domModelConfigs[mName].enabled_classes = origClasses;
-                    domModelConfigs[mName].class_configs = domModelConfigs[mName].class_configs || {};
-                    domModelConfigs[mName].class_configs[cls] = { conf: cVal, iou: iVal };
+                    domModelConfigs[norm] = domModelConfigs[norm] || {};
+                    domModelConfigs[norm].enabled_classes = activeClasses;
+                    domModelConfigs[norm].class_configs = domModelConfigs[norm].class_configs || {};
+                    domModelConfigs[norm].class_configs[cls] = { conf: cVal, iou: iVal };
 
                     domModelConfigs[clean] = domModelConfigs[clean] || {};
-                    if (origClasses) domModelConfigs[clean].enabled_classes = origClasses;
+                    domModelConfigs[clean].enabled_classes = activeClasses;
                     domModelConfigs[clean].class_configs = domModelConfigs[clean].class_configs || {};
                     domModelConfigs[clean].class_configs[cls] = { conf: cVal, iou: iVal };
                 } else {
-                    domModelConfigs[mName] = Object.assign({}, domModelConfigs[mName], { conf: cVal, iou: iVal });
-                    if (origClasses) domModelConfigs[mName].enabled_classes = origClasses;
-                    domModelConfigs[clean] = Object.assign({}, domModelConfigs[clean], { conf: cVal, iou: iVal });
-                    if (origClasses) domModelConfigs[clean].enabled_classes = origClasses;
+                    domModelConfigs[norm] = Object.assign({}, domModelConfigs[norm], { conf: cVal, iou: iVal, enabled_classes: activeClasses });
+                    domModelConfigs[clean] = Object.assign({}, domModelConfigs[clean], { conf: cVal, iou: iVal, enabled_classes: activeClasses });
                 }
             }
         });
