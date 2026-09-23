0ee3885802754b04f7d3677681e7670338295898
Charan Marripudi <charanmarripudi@github.com>
2026-09-23 19:08:08 +0530

    fix: remove nobuffer flag from server.py OPENCV_FFMPEG_CAPTURE_OPTIONS to allow clean H.265 RTSP decoding

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["TORCH_NUM_THREADS"] = "1"
 os.environ["OPENCV_FOR_THREADS_NUM"] = "1"
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;tcp|fflags;nobuffer|flags;low_delay|sync;ext|max_delay;500000|timeout;5000000"
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;tcp|timeout;5000000"
 
 import cv2
 try:
