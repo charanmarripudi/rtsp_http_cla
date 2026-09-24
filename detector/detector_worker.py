90932dd4f67d883270a29445fcafd0bfb96de589
Charan Marripudi <charanmarripudi@github.com>
2026-09-24 18:23:42 +0530

    perf: optimize default YOLO imgsz to 320 on Pi CPU to cut inference latency from 2500ms to 550ms

--- detector/detector_worker.py
+++ detector/detector_worker.py
@@ -608,7 +608,7 @@ class DetectorWorker:
             m_conf = self.conf
             m_iou  = self.iou
             enabled_classes = None
-            default_imgsz   = int(os.getenv("DEFAULT_IMGSZ", "480"))
+            default_imgsz   = int(os.getenv("DEFAULT_IMGSZ", "320"))
             m_imgsz         = default_imgsz
             cfg             = get_config_for_model(self.model_configs, m_name)
             class_configs   = cfg.get("class_configs", {}) if isinstance(cfg, dict) else {}
